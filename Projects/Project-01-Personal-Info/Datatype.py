a3a06d44279a4fa0697439ac538dc2a212f3dfad
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-08 18:24:20 +0500

    Project 01: takes personal information and print it in formatted way

---
+++ Projects/Project-01-Personal-Info/Datatype.py
@@ -0,0 +1,5 @@
+a = 1 #this is a integer
+b = 1.5 #this is a float
+c = "I am asim and i am 6 foot tall" #this is a string     
+d = True #this is a boolean
+e = None #this is a NoneType use to store a null value or no value at all
---
+++ Projects/Project-01-Personal-Info/Personal-Info.py
@@ -0,0 +1,12 @@
+# This program collects and displays personal information from the user.
+# Do not take it seriously, I am just learning.
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("\n----Personal Information----")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print("----------------------------")
