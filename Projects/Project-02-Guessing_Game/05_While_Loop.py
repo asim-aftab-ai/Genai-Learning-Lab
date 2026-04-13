3673025fda6e30c30ccd86141e0c70587969e873
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-13 14:37:45 +0500

    did while lopp excersies

---
+++ Projects/Project-02-Guessing_Game/05_While_Loop.py
@@ -0,0 +1,34 @@
+# #Excersise 1
+
+count = 5
+while count >= 1:
+    print(count)
+    count -= 1
+
+# #Excersise 2
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1  
+
+#Excersise 3
+
+count = 1
+while count <= 55:
+    print(count)
+    count += 1
+
+#Excersise 4
+
+count = 1
+while count <= 34:
+    print(count)
+    count += 1
+
+#Excersise 5
+
+count = 34
+while count >= 1:
+    print(count)
+    count -= 3
