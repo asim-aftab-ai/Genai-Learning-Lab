107fa8de4886a586ec79ff1a9d463d0c6b5d8fce
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-13 15:23:44 +0500

    did excercise of break and continue

---
+++ Projects/Project-02-Guessing_Game/07_Break_and_Contiue.py
@@ -0,0 +1,22 @@
+# Excercise 1
+for i in range(1, 11):
+    if i == 5:
+        break
+    print(i)
+# Excercise 2
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+#Excercise 3
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+#Excercise 4
+for i in range(1, 11):
+    if i % 2 != 0:
+        continue
+    print(i)
+
