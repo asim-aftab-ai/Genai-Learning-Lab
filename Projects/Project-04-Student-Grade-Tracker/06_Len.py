248e22c48c594fc7727903695f3cc4aa592c0c2d
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-14 14:41:26 +0500

    did len and len loop Excersises

---
+++ Projects/Project-04-Student-Grade-Tracker/06_Len.py
@@ -0,0 +1,12 @@
+items = ["pen", "book", "eraser"]
+
+print(len(items))  #output: 3
+
+nums = [1, 2, 3, 4, 5]
+print("Total Items:", len(nums))  #output: 5
+
+numbers = [5, 10, 15]
+for i in range(len(numbers)):
+    print("Index:", i, "Value:", numbers[i]) #output: Index: 0 Value: 5
+
+    
