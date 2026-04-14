45652d7fdaf2194cd460caef6dda98a6bf17d55f
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-14 10:48:22 +0500

    did return value excersies

--- Projects/Project-03-Calculator/03_Parameter.py
+++ Projects/Project-03-Calculator/03_Parameter.py
@@ -1,24 +1,24 @@
 #1 Excercise
 
-# def addition (num1, num2):
-#     print(num1 + num2)
+def addition (num1, num2):
+    print(num1 + num2)
 
-# addition(10, 5)
-# addition(20, 15)
-# addition(100, 50)
+addition(10, 5)
+addition(20, 15)
+addition(100, 50)
 
 #2 Excercise
 
-def addition (num1, num2):
-    print(num1 + num2)
-def subtraction (num1, num2):
-    print(num1 - num2)
-def multiplication (num1, num2):
-    print(num1 * num2)
-def division (num1, num2):
-    print(num1 / num2)
+# def addition (num1, num2):
+#     print(num1 + num2)
+# def subtraction (num1, num2):
+#     print(num1 - num2)
+# def multiplication (num1, num2):
+#     print(num1 * num2)
+# def division (num1, num2):
+#     print(num1 / num2)
 
-addition(10, 5)
-subtraction(10, 5) 
-multiplication(10, 5)
-division(10, 5)
+# addition(10, 5)
+# subtraction(10, 5) 
+# multiplication(10, 5)
+# division(10, 5)
---
+++ Projects/Project-03-Calculator/04_Return_Values.py
@@ -0,0 +1,34 @@
+#Excersise 1
+
+def add(a, b):
+  print(a +b)
+result = add(10, 5)
+print (result)
+
+#Excersise 2
+
+def multiply (a, b):
+  return a * b
+
+#Excersise 3
+
+x = multiply(2, 3)
+y = multiply(4, 5)
+print (x + y)
+
+#Excersise 4
+
+def square (num):
+  return num * num
+
+print (square(5))
+print (square(10))
+
+#Excersise 5
+
+def average (a, b):
+  return a + b / 2
+x = average(10, 20)
+y = average(5, 15)
+print (x)
+print (y)
