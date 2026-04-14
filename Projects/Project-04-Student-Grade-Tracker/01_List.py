937c82c79feb4640740bc04fb299d8574651a8d7
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-14 12:09:05 +0500

    did list excersie

---
+++ Projects/Project-04-Student-Grade-Tracker/01_List.py
@@ -0,0 +1,29 @@
+#Excersie 1
+
+Fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+print(Fruits, [0], [2], [4])
+
+#Excersise 2
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+#Excersise 3
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[0])  # Output: 1
+print(numbers[2])  # Output: 3
+print(numbers[4])  # Output: 5
+
+#Excersise 4
+
+count = [10, 20, 30, 40, 50, 60]
+print(count[0])  # Output: 10
+print(count[2])  # Output: 30
+print(count[4])  # Output: 50
+print(count[5])  # Output: 60
+
+#excersise 5
+
+Fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+print(Fruits, [0])
