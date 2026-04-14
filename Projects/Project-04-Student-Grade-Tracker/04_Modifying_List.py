51e34612393109d311929e3bce6b712705c63819
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-14 12:46:45 +0500

    did modifying excersise through indexing and slicing

---
+++ Projects/Project-04-Student-Grade-Tracker/04_Modifying_List.py
@@ -0,0 +1,27 @@
+
+# # # 👉 Change 2 to 20
+
+# # 💡 Hint:
+
+# # Index of 2 is 1
+
+# nums = [1, 2, 3, 4]
+
+# nums[1] = 20
+# print(nums)
+
+
+# # 👉 Replace 20, 30 with 99, 100
+
+# # 💡 Hint:
+
+# # Use slicing [1:3]
+
+# nums = [10, 20, 30, 40, 50]
+
+# nums[1:3] = [99, 100]  
+# print (nums)
+
+nums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+nums[1:7] = [99, 100, 110, 120, 130, 140]
+print (nums)
