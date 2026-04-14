c7f61a47f3aa6421e581e08566b65c6860f98295
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-14 15:01:25 +0500

    accessing values using keys

---
+++ Projects/Project-04-Student-Grade-Tracker/08_Accessing_Values_Using_Keys.py
@@ -0,0 +1,20 @@
+#Accessing values using keys
+
+person = {
+    "name": "Ali",
+    "age": 25
+}
+
+print(person["name"])  # Output: Ali
+print(person["age"])   # Output: 25
+
+
+car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2022
+}
+
+print(car["brand"])  # Output: Toyota
+print(car["model"])  # Output: Corolla  
+
