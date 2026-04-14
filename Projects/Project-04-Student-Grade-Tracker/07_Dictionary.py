3b9a94366ba1530675fe347487946d3cc19c41db
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-14 14:48:29 +0500

    create and practiced dictionary excersies

---
+++ Projects/Project-04-Student-Grade-Tracker/07_Dictionary.py
@@ -0,0 +1,26 @@
+car = {
+    "brand": "Toyota",
+    "year": 2020
+}
+
+# print(car) 
+
+student = {
+    "name": "Alice",
+    "age": 20,
+    "grade": "A"
+}
+
+# print(student) 
+
+basketball_player = {
+    "name": "Michael Jordan", 
+    "team": "Chicago Bulls",
+    "position": "Shooting Guard"
+}
+
+print(basketball_player)
+
+
+
+
