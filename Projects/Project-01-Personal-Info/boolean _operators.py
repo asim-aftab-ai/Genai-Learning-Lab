eafe2a9b477dcf7ea1ad754ed7a268d06f4d4da2
Asim Aftab <asim.aftab.ai@gmail.com>
2026-04-16 18:45:40 +0500

    did some excercises

---
+++ Projects/Project-01-Personal-Info/boolean _operators.py
@@ -0,0 +1,15 @@
+# Integer
+
+black_tea_grams = 14
+ginge_grams = 3
+
+total =  black_tea_grams + ginge_grams
+print(f"Total grams of base tea is: {total}")
+
+remaining_tea = black_tea_grams - ginge_grams
+print(f"Total grams of remaining tea is: {total}")
+
+milk_liters = 7
+servings = 4
+milk_per_serving = milk_liters / servings
+print(f"Milk per serving is: {milk_per_serving} liters")
---
+++ Projects/Project-01-Personal-Info/mutable_immutable.py
@@ -0,0 +1,4 @@
+a = "hello"
+
+type = type(a)
+print(type)
