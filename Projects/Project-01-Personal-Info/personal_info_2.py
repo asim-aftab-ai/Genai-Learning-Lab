5baed1ed2c8a6c0259bab7b2a331abc93b3ed372
Asim Aftab <asim.aftab.ai@gmail.com>
2026-09-01 15:39:58 +0500

    Add info collector

---
+++ Projects/Project-01-Personal-Info/personal_info_2.py
@@ -0,0 +1,13 @@
+name=input("what is your name: ")
+age=int(input("what is your age: "))
+city=input("Which city are you from: ")
+
+print("==============================")
+print("Personal Information")
+print("==============================")
+
+print(f'Name: {name}')
+print(f'age: {age}')
+print(f'City: {city}')
+print("==============================")
+
